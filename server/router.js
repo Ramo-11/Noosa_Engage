35e1615d5567794d94b7e242f246557fcc7d4548
Omar Abdelalim <omarh5877@gmail.com>
2022-11-13 15:36:55 -0500

    completed schedule tab and footer

--- server/router.js
+++ server/router.js
@@ -2,5 +2,6 @@ const express = require("express")
 const route = express.Router()
 
 route.get("/", (req, res) => res.render("index"))
+route.get("/schedule", (req, res) => res.render("schedule"))
 
 module.exports = route
