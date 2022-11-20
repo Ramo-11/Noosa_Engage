5402c76981587d8563442d694cd8320d28b68d3a
Omar Abdelalim <omarh5877@gmail.com>
2022-11-19 19:50:05 -0500

    completed staff tab

--- server/router.js
+++ server/router.js
@@ -3,5 +3,8 @@ const route = express.Router()
 
 route.get("/", (req, res) => res.render("index"))
 route.get("/schedule", (req, res) => res.render("schedule"))
+route.get("/staff", (req, res) => res.render("staff"))
+route.get("/prices", (req, res) => res.render("prices"))
+route.get("/contact", (req, res) => res.render("contact"))
 
 module.exports = route
