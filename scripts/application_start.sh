274c9ea9ad8ef9967488b48289087fa0fb3c5ddf
Omar Abdelalim <omarh5877@gmail.com>
2022-11-22 21:16:38 -0500

    now for sure it will work

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,5 +2,5 @@
 
 echo 'run application_start.sh: ' >> /home/ec2-user/Noosa_Engage/deploy.log
 
-echo 'pm2 restart noosa_engage' >> /home/ec2-user/Noosa_Engage/deploy.log
-pm2 restart noosa_engage >> /home/ec2-user/Noosa_Engage/deploy.log
+echo 'pm2 restart noosa-engage' >> /home/ec2-user/Noosa_Engage/deploy.log
+pm2 restart noosa-engage >> /home/ec2-user/Noosa_Engage/deploy.log
