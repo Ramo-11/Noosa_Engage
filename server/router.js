f394fba0801c1b56a4518710050c18e88ea4dab9
Mostafa Abdulaleem <mostafaabdulaleem@Mostafas-MacBook-Pro.local>
2024-09-08 19:30:40 -0400

    router method updated

--- server/router.js
+++ server/router.js
@@ -2,18 +2,18 @@ const express = require("express")
 const route = express.Router()
 const sendEmail = require("./mail")
 const sendAppointmentEmail = require("./appointmentEmail");
-const { getAvailableTimesRouteHandler, renderSchedulePageHandler } = require("../availableTimesHandler/availableTimesLoader"); // Import the function
+const { getAvailableTimesRouteHandler, renderSchedulePageHandler, getAvailableDatesRouteHandler } = require("../availableTimesHandler/availableTimesLoader"); // Import the function
 
 
 // *********** GET requests **********
 route.get("/", (req, res) => res.render("index"))
 route.get('/schedule', renderSchedulePageHandler);
-route.get('/api/getAvailableTimesForTutorAndDate', getAvailableTimesRouteHandler);
 route.get("/staff", (req, res) => res.render("staff"))
 route.get("/prices", (req, res) => res.render("prices"))
 route.get("/contact", (req, res) => res.render("contact"))
 
-
+route.get('/api/getAvailableTimesForTutorAndDate', getAvailableTimesRouteHandler);
+route.get('/api/getAvailableDatesForTutor', getAvailableDatesRouteHandler);
 
 // *********** POST requests **********
 route.post("/api/sendemail", sendEmail)
