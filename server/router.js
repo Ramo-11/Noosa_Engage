85cd3a9d0ba8ca5ba622386047afaf082e0a8ee0
Mostafa Abdulaleem <mostafaabdulaleem@Mostafas-MBP.attlocal.net>
2024-09-13 23:28:12 -0400

    Fixed the scheduler

--- server/router.js
+++ server/router.js
@@ -2,19 +2,14 @@ const express = require("express")
 const route = express.Router()
 const sendEmail = require("./mail")
 const sendAppointmentEmail = require("./appointmentEmail");
-const { getAvailableTimesRouteHandler, renderSchedulePageHandler, getAvailableDatesRouteHandler } = require("./availableTimesHandler/availableTimesLoader"); // Import the function
 
 
 // *********** GET requests **********
 route.get("/", (req, res) => res.render("index"))
-route.get('/schedule', renderSchedulePageHandler);
+route.get("/schedule", (req, res) => res.render("schedule"))
 route.get("/staff", (req, res) => res.render("staff"))
 route.get("/prices", (req, res) => res.render("prices"))
 route.get("/contact", (req, res) => res.render("contact"))
-
-
-route.get('/api/getAvailableTimesForTutorAndDate', getAvailableTimesRouteHandler);
-route.get('/api/getAvailableDatesForTutor', getAvailableDatesRouteHandler);
 
 // *********** POST requests **********
 route.post("/api/sendEmail", sendEmail)
