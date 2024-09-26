b2860a16a51750a3eee2a5dc0285917f20b5a70d
Mostafa Abdulaleem <mostafaabdulaleem@Mostafas-MBP.attlocal.net>
2024-09-25 20:22:06 -0400

    Great start to the login/signup

--- server/router.js
+++ server/router.js
@@ -3,7 +3,8 @@ const route = express.Router()
 const sendEmail = require("./mail")
 const sendAppointmentEmail = require("./appointmentEmail");
 const renderCoursePage = require("./courseController");
-
+const signupUser = require("./signupUser");
+const { getProfile, logout, loginUser } = require("./sessionControler");
 
 // *********** GET requests **********
 route.get("/", (req, res) => res.render("index"))
@@ -12,9 +13,16 @@ route.get("/staff", (req, res) => res.render("staff"))
 route.get("/prices", (req, res) => res.render("prices"))
 route.get("/contact", (req, res) => res.render("contact"))
 route.get('/courses/:courseName', renderCoursePage)
+route.get("/login", (req, res) => res.render("login"))
+route.get("/signup", (req, res) => res.render("signup"))
+route.get("/profile", getProfile);
+route.get('/logout', logout);
 
 // *********** POST requests **********
 route.post("/api/sendEmail", sendEmail)
 route.post("/api/scheduleAppointment", sendAppointmentEmail)
+route.post("/api/signup", signupUser);
+route.post("/api/login", loginUser);
+
 
 module.exports = route
