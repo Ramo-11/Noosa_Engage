a5b2598447429720ed8a3eaabe30d95ef0f81609
Mostafa Abdulaleem <mostafahesham346@yahoo.com>
2024-10-28 13:32:06 -0400

    First step in the forgot-password

--- server/router.js
+++ server/router.js
@@ -3,7 +3,8 @@ const route = express.Router()
 const { sendEmail } = require("./mail")
 const processScheduleRequest = require("./scheduleController")
 const renderCoursePage = require("./courseController")
-const {logout, loginUser, signupUser } = require("./session/sessionHandler")
+const { logout, loginUser, signupUser } = require("./session/sessionHandler")
+const { resetPassword, updatePassword } = require("./passwordHandler")
 
 
 // *********** GET requests **********
@@ -16,11 +17,22 @@ route.get('/courses/:courseName', renderCoursePage)
 route.get("/login", (req, res) => res.render("login"))
 route.get("/signup", (req, res) => res.render("signup"))
 route.get('/logout', logout);
+route.get("/forgotpassword", (req, res) => res.render("forgotpassword"))
+route.get('/updatepassword', (req, res) => {
+    const resetCode = req.query.code;
+    if (!resetCode) {
+        return res.status(400).send('Invalid reset code.')
+    }
+    res.render('updatepassword', { code: resetCode })
+})
 
 // *********** POST requests **********
 route.post("/api/sendEmail", sendEmail)
 route.post("/api/scheduleAppointment", processScheduleRequest)
 route.post("/api/login", loginUser)
 route.post("/api/signup", signupUser)
+route.post('/api/forgot_password', resetPassword)
+route.post('/api/update_password', updatePassword)
+
 
 module.exports = route
